aa927453f95e1fbf093041c8e41178d4e970c6f5
unknown <umairali29300293@gmail.com>
2024-04-22 18:05:35 +0500

    ado.net

--- TEK Expense Tracker/Program.cs
+++ TEK Expense Tracker/Program.cs
@@ -1,46 +1,86 @@
 ﻿using System;
 using System.Collections.Generic;
-using TEK_Expense_Tracker;
+//using TEK_Expense_Tracker;
+using System.Data;
+using System.Data.SqlClient;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Expense_Tracker tracker = new Expense_Tracker();
+        #region
+        //    Expense_Tracker tracker = new Expense_Tracker();
 
-        Console.WriteLine("\n   Expense Tracker Menu:");
-        Console.WriteLine("     1. Add Expense");
-        Console.WriteLine("     2. View Total Expenses");
-        Console.WriteLine("     3. View All Expenses");
-        Console.WriteLine("     4. Exit");
+        //    Console.WriteLine("\n   Expense Tracker Menu:");
+        //    Console.WriteLine("     1. Add Expense");
+        //    Console.WriteLine("     2. View Total Expenses");
+        //    Console.WriteLine("     3. View All Expenses");
+        //    Console.WriteLine("     4. Exit");
 
-        while (true)
+        //    while (true)
+        //    {
+        //        Console.Write("\n   Enter your choice: ");
+        //        string choice = Console.ReadLine();
+
+        //        switch (choice)
+        //        {                case "1":
+        //                Console.Write("   Enter expense description: ");
+        //                string description = Console.ReadLine();
+        //                Console.Write("   Enter expense amount: ");
+        //                int amount = int.Parse(Console.ReadLine());
+
+        //                tracker.AddExpense(description, amount);
+        //                break;
+        //            case "2":
+        //                tracker.ViewTotalExpense();
+        //                break;
+        //            case "3":
+        //                tracker.ViewAllExpense();
+        //                break;
+        //            case "4":
+        //                Console.WriteLine("Exiting...");
+        //                return;
+        //            default:
+        //                Console.WriteLine("  Invalid choice. Please enter a number from 1 to 4.");
+        //                break;
+        //        }
+        //    }
+        #endregion
+
+        Program.Connection();
+    }
+    static void Connection()
+    {
+        string cs = "Data Source=UmairA-Lap\\SQLEXPRESS;Initial Catalog=tracker;Integrated Security=true;";
+        SqlConnection mycon = null;
+        try
         {
-            Console.Write("\n   Enter your choice: ");
-            string choice = Console.ReadLine();
+            using (mycon = new SqlConnection(cs))
+            {
+               
+                mycon.Open();
+                if (mycon.State == ConnectionState.Open)
+                {
+                    Console.WriteLine("Connection has been created successfully");
+                    string query = "select * from expense";
+                    SqlCommand cm = new SqlCommand(query, mycon);
+                    SqlDataReader dr =  cm.ExecuteReader();
+                    Console.WriteLine("Expense_ID  Expense_Name  Expense_Desc  Expense_Amount  Expense_Time");
+                    while (dr.Read())
+                    {
+                        Console.WriteLine(dr["Expense_ID"]+"  "+dr["Expense_Name"] +"  "+dr["Expense_Desc"]+"  "+dr["Expense_Amount"]+"  "+dr["Expense_Time"]);
 
-            switch (choice)
-            {                case "1":
-                    Console.Write("   Enter expense description: ");
-                    string description = Console.ReadLine();
-                    Console.Write("   Enter expense amount: ");
-                    int amount = int.Parse(Console.ReadLine());
-                   
-                    tracker.AddExpense(description, amount);
-                    break;
-                case "2":
-                    tracker.ViewTotalExpense();
-                    break;
-                case "3":
-                    tracker.ViewAllExpense();
-                    break;
-                case "4":
-                    Console.WriteLine("Exiting...");
-                    return;
-                default:
-                    Console.WriteLine("  Invalid choice. Please enter a number from 1 to 4.");
-                    break;
+                    }
+                }
+               
             }
         }
+       catch(SqlException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+   
+        
     }
 }
